a91ac1c1faa0cde204d43a249580d153d5e0de7a
Manohar Kale <manoharkale5368@gmail.com>
2026-02-22 05:39:17 +0530

    icon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/image.png" type="image" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
